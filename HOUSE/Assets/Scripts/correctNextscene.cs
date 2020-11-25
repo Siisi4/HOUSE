cd00a2042d2dd5eba8ed9eb999093db8d34157ab
Simge Erdil <siisi4officiel@gmail.com>
2020-11-25 14:44:49 +0100

    next scene questions

---
+++ HOUSE/Assets/Scripts/correctNextscene.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class correctNextscene : MonoBehaviour
+{
+    public void ReponseCorrect(){
+        SceneManager.LoadScene("Langages 1");
+    }
+    
+    public void ReponseCorrectHTML(){
+        SceneManager.LoadScene("Langages");
+    }
+
+    public void ReponseCorrectCSS(){
+        SceneManager.LoadScene("Fleurs");
+    }
+}
