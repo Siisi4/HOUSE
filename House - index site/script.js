6340042a7ca1b4e833fb4160857b934047fd88b7
Daphné Delvaux <ddelvaux71@gmail.com>
2020-11-25 18:55:53 +0100

    Day 1

--- House - index site/script.js
+++ House - index site/script.js
@@ -1,1 +1,6 @@
-import { infos } from './src/data';
+// import { infos } from './src/data';
+
+const play = document.querySelector('.far');
+play.addEventListener('click', function () {
+  document.querySelector('#play').style.display = 'none';
+});
