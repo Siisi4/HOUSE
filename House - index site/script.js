6d1fb431ad5fc66e6e101e4fb9a00a600eb1c03b
Daphné Delvaux <ddelvaux71@gmail.com>
2020-11-26 13:45:28 +0100

    push/pull pour parcel

--- House - index site/script.js
+++ House - index site/script.js
@@ -1,22 +1,30 @@
 // import { infos } from './src/data';
 
 const infos = [{
-  nom: 'Shin-Yuh', formation: 'WAD', image: 'av_shinyuh.png', texte: 'Bonjour,  je suis Shin-Yuh! Une ‘junior web developer’ en formation! Toujours désireuse d"apprendre de nouvelles choses et espérer construire un meilleur monde par la programmation!', lien: '',
+  nom: 'Shin-Yuh', formation: 'WAD', image: 'av_shinyuh.png', texte: 'Bonjour,  je suis Shin-Yuh! Une ‘junior web developer’ en formation! Toujours désireuse d\'apprendre de nouvelles choses et espérer construire un meilleur monde par la programmation!', lien: '',
 }, {
-  nom: 'Siisi4', formation: 'Game', image: 'av_siisie.png', texte: 'Yo ! Moi c"est Siisi4, développeuse de jeux-vidéo Jr. passionnée.. ♥ Je suis également streameuse de variétés à mes heures perdues sur la platforme Twitch. J"aime les jeux et les chats !  °bois son verre de lait °', lien: 'https://siisi4.itch.io',
+  nom: 'Siisi4', formation: 'Game', image: 'av_siisie.png', texte: 'Yo ! Moi c\'est Siisi4, développeuse de jeux-vidéo Jr. passionnée.. ♥ Je suis également streameuse de variétés à mes heures perdues sur la platforme Twitch. J\'aime les jeux et les chats !  °bois son verre de lait °', lien: 'https://siisi4.itch.io',
 }, {
-  nom: 'Annick', formation: 'WEB', image: 'av_annick.png', texte: '', lien: '',
+  nom: 'Annick', formation: 'WEB', image: 'av_annick.png', texte: 'Dynamique et souriante, je suis à la recherche de nouveaux challenges dans l’administration. Je suis active, flexible et débrouillarde et je me montre particulièrement motivée dans ce que j’entreprends. Fan de jeux vidéos et de la couleur mauve , je m\'adapte à toute situation pour n\'en sortir qu\'avec le meilleur. En gros, Go and Have Fun', lien: '',
 }, {
-  nom: 'Daphné', formation: 'WEB', image: 'av_daphne.png', texte: 'Coucou, je suis en pleine découverte et je trouve cela génial! Débarquer dans ce monde de l"informatique avec cette formation de front-end developer, ça me change ! Que l"exploration continue!', lien: '',
+  nom: 'Daphné', formation: 'WEB', image: 'av_daphne.png', texte: 'Coucou, je suis en pleine découverte et je trouve cela génial! Débarquer dans ce monde de l\'informatique avec cette formation de front-end developer, ça me change ! Que l\'exploration continue!', lien: '',
 }, {
-  nom: 'Claudia', formation: 'Game', image: 'av_claudia.png', texte: 'My name is Claudia and I"m a girl who likes to play a lot. I also have an obsession with cats and cute stuff.', lien: '',
+  nom: 'Claudia', formation: 'Game', image: 'av_claudia.png', texte: 'My name is Claudia and I\'m a girl who likes to play a lot. I also have an obsession with cats and cute stuff.', lien: '',
 }];
 
-let html = '<div class="presentation">';
+let html = '';
 for (const info of infos) {
-  html += '<div><img src="./static" />';
+  html += `
+  <div class="info">
+    <img src="./static/${info.image}" />
+    <h3>${info.nom}</h3>
+    <h5>${info.formation}</h5>
+    <p>${info.texte}</p>
+    <a href="${info.texte}">Vers mon Portfolio</a>
+  </div>`;
 }
-html += '</div>';
+
+document.querySelector('.presentation').innerHTML = html;
 
 document.querySelector('#intro').style.display = 'block';
 document.querySelector('#explicatif').style.display = 'none';
@@ -25,6 +33,7 @@ document.querySelector('#conceptrices').style.display = 'none';
 
 const btnIntro = document.querySelector('#btnIntro');
 btnIntro.addEventListener('click', function () {
+  btnIntro.style.textDecorationLine = 'underline';
   document.querySelector('#intro').style.display = 'block';
   document.querySelector('#explicatif').style.display = 'none';
   document.querySelector('#jeu').style.display = 'none';
--- House - index site/src/data.js
+++ House - index site/src/data.js
@@ -3,7 +3,7 @@ export const infos = [{
 }, {
   nom: 'Siisi4', formation: 'Game', image: 'av_siisie.png', texte: 'Yo ! Moi c"est Siisi4, développeuse de jeux-vidéo Jr. passionnée.. ♥ Je suis également streameuse de variétés à mes heures perdues sur la platforme Twitch. J"aime les jeux et les chats !  °bois son verre de lait °', lien: 'https://siisi4.itch.io',
 }, {
-  nom: 'Annick', formation: 'WEB', image: 'av_annick.png', texte: '', lien: '',
+  nom: 'Annick', formation: 'WEB', image: 'av_annick.png', texte: 'Dynamique et souriante, je suis à la recherche de nouveaux challenges dans l’administration. Je suis active, flexible et débrouillarde et je me montre particulièrement motivée dans ce que j’entreprends. Fan de jeux vidéos et de la couleur mauve , je m"adapte à toute situation pour n"en sortir qu"avec le meilleur. En gros, Go and Have Fun', lien: '',
 }, {
   nom: 'Daphné', formation: 'WEB', image: 'av_daphne.png', texte: 'Coucou, je suis en pleine découverte et je trouve cela génial! Débarquer dans ce monde de l"informatique avec cette formation de front-end developer, ça me change ! Que l"exploration continue!', lien: '',
 }, {
