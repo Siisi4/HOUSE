cd4857bb7ba914763ee796cccef66949a772c567
Simge Erdil <siisi4officiel@gmail.com>
2020-11-27 12:13:42 +0100

    build test

--- HOUSE/Assets/Scripts/SceneSwitcher.cs
+++ HOUSE/Assets/Scripts/SceneSwitcher.cs
@@ -33,4 +33,8 @@ public class SceneSwitcher : MonoBehaviour
         SceneManager.LoadScene("Chat");
     }
 
+    public void SwitchtoQuestions(){
+        SceneManager.LoadScene("phraseDeDev");
+    }
+
 }
